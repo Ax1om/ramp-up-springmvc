30b9edc65c1b1d0ed0a54e5dae3f3a77af1930a5
Mauricio Enriquez <mauricio.enriquez@nestle.com>
2017-08-27 17:26:25 +0200

    SpringMVC - Boilerplate

---
+++ src/main/java/com/nrqapps/springmvc/config/AppConfig.java
@@ -0,0 +1,49 @@
+package com.nrqapps.springmvc.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.nrqapps.springmvc")
+public class AppConfig extends WebMvcConfigurerAdapter{
+
+    // Best method to configure view resolver as the IDE will recognize the views used on the controllers
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    /**
+     * Configure ResourceHandlers to serve static resources like CSS/ Javascript etc...
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
+
+
+    /**
+     * Configure MessageSource to lookup any validation/error message in internationalized property files
+     */
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,10 @@
+<%--@elvariable id="message" type="java.lang.String"--%>
+<html>
+    <head>
+    </head>
+    <body>
+        <div class="container">
+            <h1>${message}</h1>
+        </div>
+    </body>
+</html>
